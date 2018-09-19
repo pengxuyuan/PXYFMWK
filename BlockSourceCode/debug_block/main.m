7813ed25958fdb9c20021442ff250ef2a625dd18
Pengxuyuan <ba451946155@163.com>
2018-09-19 18:03:43 +0800

    增加 Block Source Code，可以断点看 Block 内部执行的流程； 可以很方便的研究 MRC，ARC；以及 栈Block、堆Block 创建执行的流程，以及 __block 的操作流程。

---
+++ BlockSourceCode/debug_block/main.m
@@ -0,0 +1,53 @@
+//
+//  main.m
+//  debug_block
+//
+//  Created by 八点钟学院 on 2017/11/1.
+//
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#import <Block.h>
+
+typedef void(^EOCBlock)(void);
+
+//block源代码：https://opensource.apple.com/source/libclosure/libclosure-65/
+//工程的搭建参考：http://blog.csdn.net/wotors/article/details/52489464
+//main.m 转成C++的代码命令（xcode 9.4下）：xcrun -sdk macosx10.13 clang -S -rewrite-objc -fobjc-arc main.m
+
+///分析源代码的目的：
+//block本身是在栈，当在arc下，赋值给strong或copy修饰的block变量，block会自动从栈拷贝到堆区，这个流程是怎么样的？
+//1、__block修饰的外部变量
+//2、非__block修饰的外部变量
+//这个过程会明白到block结构里的一些变量到底起什么作用
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        
+        int a[5] = {1,2,3,4,5};
+        int *ptr = (int *)(&a + 1);
+        printf("%d", *(ptr-1));
+        
+        __block NSButton *btn = [[NSButton alloc] init];
+        
+        void (^block)(void) = Block_copy( ^{
+            
+            NSLog(@"这是自己的block %@", btn);
+            
+        });
+        
+        block();
+        
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+//-fno-objc-arc
